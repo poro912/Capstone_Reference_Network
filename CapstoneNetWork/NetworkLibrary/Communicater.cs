5474f7d1965b7296598ad668b420c40ae1230f98
poro912 <40479447+poro912@users.noreply.github.com>
2022-11-28 01:57:28 +0900

    Bug fix attempt 1

--- CapstoneNetWork/NetworkLibrary/Communicater.cs
+++ CapstoneNetWork/NetworkLibrary/Communicater.cs
@@ -169,7 +169,7 @@ namespace Protocol
 				// 무조건 1024 바이트로만 데이터를 전소한다.
 				// 추후 1024 바이트를 넘기는 데이터를 받을 수 있게 되면 이곳을 변경해야 한다. 
 				this.stream.Write(data, 0, data.Length);
-				this.stream.Write(zeroByte, 0, 1023 - data.Length);
+				this.stream.Write(zeroByte, 0, 1024 - data.Length);
 			}
 			catch (Exception e)
 			{
@@ -264,8 +264,9 @@ namespace Protocol
 			Console.WriteLine("Communicater\t: 데이터 들어옴");
 			Console.WriteLine("Communicater\t: 데이터 길이 : " + received_byte.Length);
 			string receive_data = Encoding.Default.GetString(this.received_byte);
-			Console.WriteLine("Communicater\t: 내용 : " + receive_data);
+			Console.WriteLine("Communicater\t: 내용 : " + receive_data + "|");
 
+			/*
 			if (received_byte[0] == 0)
 			{
 				StopReceive();
@@ -276,7 +277,8 @@ namespace Protocol
 				StopReceive();
 				return;
 			}
-				
+			*/
+
 			// 데이터를 받아오는 대로 queue에 저장
 			receive_queue.Enqueue(this.received_byte);
 			received_byte = new byte [1024]; 
